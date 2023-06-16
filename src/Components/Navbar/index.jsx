bd8502c4476492acd591c2a15e7a4ac17d0e6a4f
luis hilario huaman <hilario.huaman.6@gmail.com>
2023-06-16 16:05:21 -0500

    adding 'position: sticky' to navBar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -45,7 +45,7 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <Disclosure as="nav" className="">
+    <Disclosure as="nav" className="sticky top-0 z-10 bg-white">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
